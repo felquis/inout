dc2c6e8e9cbb0693d782bf0bbc66f67ceb8a3d2a
Felquis Gimenes <frgformenton@gmail.com>
2015-08-16 08:12:12 -0300

    Fix Indent and Remove Let It Go function :snowman:
    Now, I really think it's awesome.
    
    I Figured out that LetItGo is actually the function In it self.
    
    So, if Transform is defined. It returns a new In() with the transformed
    data.
    
    If transform is NOT defined. It returns a new In() with the current data
    
    And now we can do stuff like
    
    new In(1)(log)(add(2))(log) // output 3
    
    Discussed here
    https://github.com/felquis/inout/commit/fe00815cf7ffea1762501a3f36bf5783
    7eca2fe9#commitcomment-12726247

--- index.js
+++ index.js
@@ -3,29 +3,22 @@
 * @param {[type]} data to be changed
 */
 function In(data) {
+    function Out(Transform) {
+        return Transform? In((Transform)(data)) : In(data)
+    }
 
-	function Out(func) {
-		return new In( (func || letItGo)(data) );
-	}
-
-	return Out;
-}
-
-function letItGo(data) {
-	console.log('letItGo');
-
-	return data;
+    return Out
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports = In;
+    module.exports = In
 } else {
-	if (typeof define === 'function' && define.amd) {
-		define([], function() {
-			return In;
-		});
-	}
-	else {
-		window.In = In;
-	}
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return In
+        });
+    }
+    else {
+        window.In = In
+    }
 }
